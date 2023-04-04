cc8f594b18ec84f264d2912e43f6bcbe9ca74c59
Owen Huang <ohuang2@gmail.com>
2023-04-03 22:25:21 -0400

    Added a user personal info activity page.

---
+++ app/src/main/java/edu/northeastern/finalprojectcs5520/UserPersonalInfo.java
@@ -0,0 +1,14 @@
+package edu.northeastern.finalprojectcs5520;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserPersonalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_personal_info);
+    }
+}
