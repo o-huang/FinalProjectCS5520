ba038574dd7f597efa574eb5d769fd4c91828411
Ingrid Liu <todearannie@gmail.com>
2023-04-07 21:06:44 -0700

    user display page layout

--- app/src/main/java/edu/northeastern/finalprojectcs5520/UserDisplay.java
+++ app/src/main/java/edu/northeastern/finalprojectcs5520/UserDisplay.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
